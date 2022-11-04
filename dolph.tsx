fd0b6515853c7e47f068551ad0aea3366504fad5
bwoexe <bretwardo@gmail.com>
2022-11-03 20:05:08 -0700

    camera test

---
+++ dolph.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="dolph" tilewidth="24" tileheight="24" tilecount="20" columns="10">
+ <image source="source\img\dolphbg-table-24-24.png" width="240" height="48"/>
+</tileset>
